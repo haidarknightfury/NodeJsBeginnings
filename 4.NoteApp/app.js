ee04cb78a0c453a95c543ebb04c8b83b457e57ae
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-04-29 16:52:06 +0400

    new note application + add yargs

--- 4.NoteApp/app.js
+++ 4.NoteApp/app.js
@@ -1,8 +1,14 @@
 
 const FS  = require('fs');
+const Yargs = require('yargs');
 
 const Note = require('./note.js');
 const _ = require('lodash');
+
+const argv= Yargs.argv;
+
+console.log('process argv',process.argv);
+console.log('yarns argv',argv);
 
 console.log('starting app');
 Note.getNotes();
@@ -10,21 +16,25 @@ Note.getNotes();
 var arr = [1,2,3,3,4,4,5,5];
 console.log(_.uniq(arr));
 
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command :',command);
 
 switch(command){
     case 'add':
         console.log('Adding new note');
+        Note.addNote(argv.title,argv.body);
         break;
     case 'list':
         console.log('listing notes');
+        Note.getAll();
         break;
     case 'read':
         console.log('read note');
+        Note.read(argv.title);
         break;
     case 'remove':
         console.log('remove note');
+        Note.remove(argv.title);
         break;
     default:
         console.log('not recognized')
--- 4.NoteApp/note.js
+++ 4.NoteApp/note.js
@@ -1,5 +1,28 @@
 console.log('starting note');
 
-module.exports.getNotes=()=>{
+var getNotes=()=>{
     console.log('returning notes');
+}
+
+var addNote=(title,body)=>{
+    console.log(`Note with title ${title} and body ${body} added`);
+}
+var getAll = ()=>{
+    console.log(`printing all the notes`);
+};
+
+var read=(title)=>{
+    console.log(`retrieving note with title ${title}`);
+}
+
+var remove = (title) => {
+    console.log(`removing note with title ${title}`);
+}
+
+module.exports ={
+    getNotes:getNotes,
+    addNote:addNote,
+    getAll,
+    read,
+    remove
 }

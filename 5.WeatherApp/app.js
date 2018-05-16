ba2e092cf28f3af55c9cff99884827769fe90014
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-05-16 22:13:36 +0400

    use axios

--- 5.WeatherApp/app.js
+++ 5.WeatherApp/app.js
@@ -1,39 +1,43 @@
-const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
+const axios = require('axios');
 
+
+//axios
 
 const argv = yargs.options({
-    a:{
-        demand:true,
-        alias:'address',
-        describe:'address to fetch the weather',
-        string: true
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'address to fetch the weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geoCodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBXMPJaJY9CQydF9NQn56gaToHPiB2eTys`;
+axios.get(geoCodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
     }
+    var lat = response.data.results[0].geometry.location.lat;
+    var long = response.data.results[0].geometry.location.lng;
+    debugger;
+    var weatherURL = `https://api.darksky.net/forecast/3599e33536f00c5033bb9f3a15463c67/${lat},${long}`;
+    return axios.get(weatherURL);
+
+}).then((response)=>{
+    console.log(response.data.currently.temperature);
 })
-.help()
-.alias('help','h')
-.argv;
-
-// encodeURIComponent - encode url
-// decodeURIComponent - decode the encoded url
-
-var geocodeCallback = (errorMessage, result) =>{
-      if (errorMessage) {
-          console.log(errorMessage);
-      } else {
-          console.log(JSON.stringify(result, undefined, 2));
-          weather.getWeather(result.latitude, result.longitude, weatherCallback);
-      }
-}
-
-var weatherCallback = (errorMessage,result) =>{
-       if (errorMessage) {
-           console.log(errorMessage);
-       } else {
-           console.log(JSON.stringify(result, undefined, 2));
-       }
-}
-
-geocode.geocodeAddress(argv.address, geocodeCallback);
+.catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log('not found');
+    } else {
+        console.log(e.message);
+    }
+});

f2961774a01901591d438b68a86d6449a54a2317
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-05-19 11:53:30 +0400

    using partials using hbs

--- 6.NodeWebServer/server.js
+++ 6.NodeWebServer/server.js
@@ -1,22 +1,45 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
 
-// express middleware
-app.use(express.static(__dirname+ '/public'));
+
+// express middleware // http://localhost:3000/help.html
+// handlebars - dynammic templating
+app.use(express.static(__dirname + '/public'));
+
+// helper to be available for all hbs files
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(str)=>{
+    return str.toUpperCase();
+})
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'haidar',
-        age: '16',
-        likes: ['programming', 'anime']
-    });
+    // res.send({
+    //     name: 'haidar',
+    //     age: '16',
+    //     likes: ['programming', 'anime']
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage : `welcome haidar`
+    })
 });
 
 
 app.get('/about', (request, response) => {
-    response.send(`About page`);
+    //response.send(`About page`);
+    response.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -25,6 +48,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server is up on port 3000');
 });

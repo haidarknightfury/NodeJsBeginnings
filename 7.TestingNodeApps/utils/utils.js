b42106b7ade929706285b07e3cbb17e9d7e76d72
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-05-20 19:20:05 +0400

    include mocha and expect

---
+++ 7.TestingNodeApps/utils/utils.js
@@ -0,0 +1,11 @@
+module.exports.add = (a, b) => {
+    return a + b;
+};
+module.exports.square = (x) => x * x;
+
+module.exports.setName = (user, fullName)=>{
+    var names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastname = names[1];
+    return user;
+}
---
+++ 7.TestingNodeApps/utils/utils.test.js
@@ -0,0 +1,60 @@
+const utils = require('./utils');
+const expect = require('expect');
+
+// call mocha in package.json - test
+it("should add two numbers", () => {
+    var result = utils.add(33, 11);
+    if (result != 44) {
+        // test fail
+        throw new Error(`Expected 44 but returned ${result}`);
+    }
+});
+
+it("should square a number", () => {
+    var rs = utils.square(9);
+    // if (rs != 81) {
+    //     throw new Error(`Expected 81 but returned ${rs}`);
+    // }
+    expect(rs).toBe(81).toBeA('number');
+});
+
+it("should not be 80", () => {
+    var rs = utils.square(3);
+    expect(rs).toNotBe(10);
+    expect({
+        name: 'Andrew'
+    }).toEqual({
+        name: 'Andrew'
+    });
+    expect([2, 3, 4]).toInclude([2]);
+    expect([2, 3, 4]).toExclude(5);
+    expect({
+        name: 'haidar',
+        age: 25
+    }).toInclude({
+        age: 25
+    })
+    expect({
+        name: 'haidar',
+        age: 25
+    }).toExclude({
+        age: 26
+    })
+});
+
+it('should produce user given user and names', () => {
+    var oldUser ={location:'gs'};
+     var User = utils.setName(oldUser, 'haidar dargaye');
+    expect(User).toInclude({
+        lastname: 'dargaye'
+    }).toBeA('object');
+
+    expect(User).toEqual(oldUser);// by reference
+    
+});
+
+
+//nodemon --exec "npm test"
+// automatically reloads test on changes
+// npm run test-watch -add test-watch: "nodemon --exec \"npm test\"" in package.json
+//toBe uses tripple equal to check

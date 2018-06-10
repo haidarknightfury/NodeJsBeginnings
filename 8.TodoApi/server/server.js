1ad2302b37952f33499ffcd932f2c2c849930f6e
Haidar <haidar.dargaye@umail.uom.ac.mu>
2018-06-10 16:04:08 +0400

    add server code

--- 8.TodoApi/server/server.js
+++ 8.TodoApi/server/server.js
@@ -1,25 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 
-var newTodo = new Todo({
-    text: 'cooked dinner',
-    completed: 'false',
-    completedAt: 1234
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    console.log(req.body);
+    var todo = new Todo({
+        text:req.body.text
+    });
+    todo.save().then((result)=>{
+        console.log('successfully saved the todo : ',todo);
+        res.status(200).send(todo);
+    },(e)=>{
+        console.log('error in saving the todo');
+        res.status(400).send(e);
+    });
 });
 
-newTodo.save().then((result) => {
-    console.log('saved todo', result);
-}, (e) => {
-    console.log('error', e);
+
+app.get('/todos',(req,res)=>{
+    
 });
 
-var newUser = new User({
-    email: 'haidardargaye@gmail.com'
+app.listen(3000, () => {
+    console.log('started on port 3000');
 });
 
-newUser.save().then((result) => {
-    console.log('saved user', result);
-}, (e) => {
-    console.log('error', e);
-});
+
+// var newTodo = new Todo({
+//     text: 'cooked dinner',
+//     completed: 'false',
+//     completedAt: 1234
+// });
+
+// newTodo.save().then((result) => {
+//     console.log('saved todo', result);
+// }, (e) => {
+//     console.log('error', e);
+// });
+
+// var newUser = new User({
+//     email: 'haidardargaye@gmail.com'
+// });
+
+// newUser.save().then((result) => {
+//     console.log('saved user', result);
+// }, (e) => {
+//     console.log('error', e);
+// });
